55ba1cdc8ba1278fafbc943dd4d914d31943d2e9
jamesmartherus <james@martherus.com>
2019-08-21 21:30:25 -0500

    Initial Commit

---
+++ R/beat.R
@@ -0,0 +1,124 @@
+#'Play a drum sound
+#'
+#'\code{beat} plays a drum beat.
+#'
+#'
+#'@param drum character string or number specifying which drum is to be played by 
+#'  specifying one of the built in sounds. The default is "snare". Possible sounds are:
+#'  \enumerate{ \item \code{"kick"} \item \code{"snare"} \item \code{"hihat"}
+#'  \item \code{"crash"}} If \code{drum} does not match any
+#'  of the sounds above, a random sound will be played.
+#'  
+#'@param kit character string specifying which kit is to be used. The default is "acoustic."
+#' Possible kits are:
+#' \enumerate{ \item \code{"acoustic"} \item \code{"hiphop"} \item \code{"electro"}}
+#'  If \code{kit} does not match any
+#'  of the sounds above, a random sound will be played.
+#'  
+#'@param expr An optional expression to be excecuted before the sound.
+#'
+#'  
+#'  
+#'@return NULL
+#'  
+#' @examples
+#' # Play a "snare" sound from the "acoustic" kit
+#' beat()
+#' 
+#' \dontrun{
+#' # Play a bass sound from the hiphop kit.
+#' beep(drum="bass", kit="hiphop")
+#' 
+#' }
+#'@export
+beat <- function(drum="snare", kit="acoustic", expr=NULL) {
+  expr
+  drums <- c(snare = "snare.wav",
+              kick = "kick.wav",
+              hihat = "hihat.wav",
+              crash = "crash.wav")
+  kits <- c(acoustic = "acoustic/",
+            electro = "electro/",
+            hiphop = "hiphop/")
+  drum_path <- NULL
+  if(is.na(drums[drum]) || length(drums[drum]) != 1 || is.na(kits[kit]) || length(kits[kit]) != 1) {
+    if(is.character(drum)) {
+      drum <- str_trim(drum)
+      if(is.character(kit)){
+        kit <- str_trim(kit)
+        if(file.exists(system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr"))){
+          drum_path <- system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr")
+        } else {
+          warning(paste('"', drum, '" is not a valid drum, playing a random sound instead.', sep = ""))
+        }
+      }
+    }
+  } else if(is.na(kits[kit]) || length(kits[kit]) != 1) {
+    if(is.character(kit)) {
+      kit <- str_trim(kit)
+      if(file.exists(kit)) {
+        drum_path <- drum
+      } else {
+        warning(paste('"', drum, '" is not a valid drum, playing a random sound instead.', sep = ""))
+      }
+    }
+  } else {
+    drum_path <- system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr")
+  }
+  
+  if(is.null(drum_path)) { # play a random sound
+    drum_path <- system.file(paste("sounds/", sample(kits, size=1), sample(drums, size=1), sep=""), package="drumr")
+  }
+  
+  tryCatch(play_file(drum_path), error = function(ex) {
+    warning("beat() could not play the sound due to the following error:\n", ex)
+  })
+}
+
+is_wav_fname <- function(fname) {
+  str_detect(fname, regex("\\.wav$", ignore_case = TRUE))
+}
+
+escape_spaces <- function(s) {
+  str_replace_all(s, " ", "\\\\ ")
+}
+
+play_vlc <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit ", fname), 
+         ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_paplay <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_aplay <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("aplay --buffer-time=48000 -N -q ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_audio <- function(fname) {
+  sfx <- load.wave(fname)
+  play(sfx)
+}
+
+play_file <- function(fname) {
+  if(Sys.info()["sysname"] == "Linux") {
+    if(is_wav_fname(fname) && nchar(Sys.which("paplay")) >= 1) {
+      play_paplay(fname)
+    } else if(is_wav_fname(fname) && nchar(Sys.which("aplay")) >= 1) {
+      play_aplay(fname)
+    } else if(nchar(Sys.which("vlc")) >= 1) {
+      play_vlc(fname)
+    } else {
+      play_audio(fname)
+    }
+  } else {
+    play_audio(fname)
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # drumr
 Turn R into a Drum Machine
+
+
+Drum samples courtesy of musicradar.com:
+https://www.musicradar.com/news/drums/sampleradar-1000-free-drum-samples-229460
+
+
+
+
