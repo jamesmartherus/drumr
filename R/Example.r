ee9ea66a4a37edcd58c765fda67bc94d42a640e5
jamesmartherus <james@martherus.com>
2019-09-04 08:15:50 -0500

    Add Example of a Tasty Beat

---
+++ R/Example.r
@@ -0,0 +1,62 @@
+# Bars 1-3
+for(i in 1:3){
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="snare",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(120)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="snare",kit="acoustic")
+  
+  tempo(120)
+  
+}
+
+#Bar 4
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(240)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(120)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(480)
+
+beat(drum="snare",kit="acoustic")
+
+
+
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # drumr
 Turn R into a Drum Machine
 
-This package allows you to play drum beats from within R. At present the package contains only one working function -- `beat`, which plays a beat from a given `kit` and `drum`. Admittedly, this package is basically useless, but it was fun to write. 
+This package allows you to play drum beats from within R. At present the package contains only two working functions -- `beat`, which plays a beat from a given `kit` and `drum`, and `tempo`, which sets the spacing between beats. Admittedly, this package is basically useless, but it was fun to write. 
 
+For an example of a tasty beat, see Example.R
 
 Drum samples courtesy of [sampleswap.org](sampleswap.org)
 
